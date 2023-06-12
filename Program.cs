95ae2298bb6982f688a57f19fa49bf86ba42a1ca
Michael Geiger <michael.geiger@hp.com>
2023-06-12 14:44:16 -0700

    commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClosedXML.Excel;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ws = new XLWorkbook("test.xlsx");
+        ws.SaveAs("test-out.xlsx");
+    }
+}
